b4cddb3229716916344cb408a7dbc23792f596a3
H-N-S-Abayawardhana <hnsabayawardhana@gmail.com>
2025-05-13 15:06:28 +0530

    Updated App and Main .jsx files

---
+++ my-tech-blog/src/config.js
@@ -0,0 +1,25 @@
+const config = {
+  // Blog Information
+  title: "My Tech Blog",
+  description: "A blog about web development, programming, and tech insights",
+  author: "Niwarthana Abayawardhana",
+  siteUrl: "https://yourdomain.com",
+  
+  // Social Media
+  social: {
+    twitter: "yourtwitterhandle",
+    github: "yourgithubhandle",
+    linkedin: "yourlinkedinname",
+  },
+  
+  // Navigation
+  navigation: [
+    { name: 'Home', path: '/' },
+    { name: 'About', path: '/about' },
+  ],
+  
+  // Date Formats
+  dateFormat: 'MMMM dd, yyyy',
+}
+
+export default config
